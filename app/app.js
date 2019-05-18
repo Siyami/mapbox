fa7d3c4efa2c9009c3f68f453888a99da509b2c2
Siyami Avci <siyamiavci@gmail.com>
2019-05-18 12:09:36 -0700

    add popup to markers, add styling and clean up the code

--- app/app.js
+++ app/app.js
@@ -2,82 +2,91 @@ import './app.scss';
 
 let MAPBOX_TOKEN = 'pk.eyJ1IjoiaW50ZWdyYWxnaXMiLCJhIjoiY2plajNiOXAwM2RhbTJwbG45cnYyd2kycyJ9.5ayhl9GwdjpTpC1zE8A0xg';
 let MAPBOX_STYLE = 'mapbox://styles/integralgis/cjnf43ayw05dp2tn034rr5mk2';
-let people = [];
-let peopleUnderAge30 = [];
+let apiUrl = 'https://cors-anywhere.herokuapp.com/https://apps.integralgis.com/node/integralgis/people.json';
+let employees = [];
 
 mapboxgl.accessToken = MAPBOX_TOKEN;
-var map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
     container: 'map',
     style: MAPBOX_STYLE,
-    // center: [87.485721, -22.168051],
     zoom: 1
 });
 
-
-/////
-// var map = new mapboxgl.Map({
-//     container: 'map', // HTML container id
-//     style: 'mapbox://styles/mapbox/streets-v9', // style URL
-//     center: [-21.92661562, 64.14356426], // starting position as [lng, lat]
-//     zoom: 13
-//   });
-  
-//   var popup = new mapboxgl.Popup()
-//   .setHTML('<h3>Reykjavik Roasters</h3><p>A good coffee shop</p>');
-  
-//   var marker = new mapboxgl.Marker()
-//   .setLngLat([-21.92661562, 64.14356426])
-//   .setPopup(popup)
-//   .addTo(map);
-
-function getPeople(url, success) {
-    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    xhr.open('GET', url);
+function getPeople(url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET',url);
     xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+        if (xhr.readyState>3 && xhr.status==200) callback(xhr.responseText);
     };
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
-    return xhr;
+    return xhr; 
 }
 
 function filterList(filter) {
     let filteredList = [];
     let div = document.getElementById('emp');                
+    let child = div.lastElementChild;  
 
-    var child = div.lastElementChild;  
+    // remove if a list exists
     while (child) { 
         div.removeChild(child); 
         child = div.lastElementChild; 
     } 
 
-    for(let el of people) {
+    for(let el of employees) {
         if(el.age < filter) filteredList.push(el);
     }
+    // Sort filtered array by name to make it user friendly
+    filteredList.sort((a, b) => {
+        var nameA = a.name.toUpperCase(); 
+        var nameB = b.name.toUpperCase(); 
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        // names must be equal
+        return 0;
+    });
     
     let list = document.createElement('ul'); 
+    if(filteredList.length === 0) {
+        let li = document.createElement('li');
+        li.innerText = 'N/A';
+        list.appendChild(li);
+    }
     for(let el of filteredList) {
         let item = document.createElement('li');
-        item.appendChild(document.createTextNode(el.name));
+        item.innerText = el.name;
         list.appendChild(item);
     }
-    document.getElementById('emp').appendChild(list);
+    div.appendChild(list);
 }
 
 let range = document.getElementById('range');
+
 range.addEventListener('change', function(event) {
     let fil = event.target.value;
-    document.getElementById('filter-text').innerText = `Show ages ${fil} and under`;
+    document.getElementById('filter-text').innerText = `Employees younger than ${fil} years old`;
     filterList(fil);
 });
 
-getPeople('https://cors-anywhere.herokuapp.com/https://apps.integralgis.com/node/integralgis/people.json', function(data){
-    people = JSON.parse(data);
-    for(let el of people) {
-        var marker = new mapboxgl.Marker()
+function setMarkers() {
+    for(let el of employees) {
+        let popup = new mapboxgl.Popup()
+        .setHTML(`
+            <p>Name: ${el.name}</p>
+            <p>Age: ${el.age}</p>
+        `);
+        
+        let marker = new mapboxgl.Marker()
         .setLngLat([el.longitude, el.latitude])
-        .addTo(map);
+        .setPopup(popup)
+        .addTo(map);     
     }
+}
 
+getPeople(apiUrl, function(data) { 
+    employees = JSON.parse(data);
+    
+    setMarkers(employees);
     filterList(30);
 });
